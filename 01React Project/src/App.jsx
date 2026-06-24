3ee8c7fee41e9067ac59f523e1494cb7e6b9b29d
Aneela-Fatima <aneelafatima369@gmai.com>
2026-06-24 17:47:45 +0500

    Initial React Project Setup

---
+++ 01React Project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><h1>Hello, React! This is my first React app.</h1></>
+  )
+}
+
+export default App
